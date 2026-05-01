fa3a52ccef4c28e3286215fbc571047feb7ff1c0
lucasarge <sargentl@stu.otc.school.nz>
2026-05-01 14:15:01 +1200

    Almost completed Register HTML.

--- clarity/users/forms.py
+++ clarity/users/forms.py
@@ -1,7 +1,13 @@
 from django.contrib.auth.forms import UserCreationForm
 from .models import CustomUser
+from django import forms
 
 class RegisterForm(UserCreationForm):
     class Meta:
         model = CustomUser
         fields = ("first_name", "last_name", "user_type", "email", "phone", "password1", "password2")
+        widgets = {
+            'first_name': forms.TextInput(attrs={'placeholder': 'John'}),
+            'last_name': forms.TextInput(attrs={'placeholder': 'Doe'}),
+            'password1': forms.PasswordInput(attrs={'placeholder': 'Password'}),
+        }
