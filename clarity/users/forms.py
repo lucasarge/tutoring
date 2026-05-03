62c32e7eafeb9d67ae9659563cfc9563d684e54f
luca_sarge <sargentl@stu.otc.school.nz>
2026-05-03 13:25:43 +1200

    Half of Profile page is complete. Working on Caregiver Connect feature next.

--- clarity/users/forms.py
+++ clarity/users/forms.py
@@ -1,5 +1,5 @@
 from django.contrib.auth.forms import UserCreationForm
-from .models import CustomUser
+from .models import CustomUser, Profile
 from django import forms
 
 class RegisterForm(UserCreationForm):
@@ -10,4 +10,20 @@ class RegisterForm(UserCreationForm):
             'first_name': forms.TextInput(attrs={'placeholder': 'John'}),
             'last_name': forms.TextInput(attrs={'placeholder': 'Doe'}),
             'password1': forms.PasswordInput(attrs={'placeholder': 'Password'}),
+        }
+
+class UpdateUserForm(forms.ModelForm):
+    class Meta:
+        model = CustomUser
+        fields = ("first_name", "last_name", "email", "phone")
+
+class ProfileImageForm(forms.ModelForm):
+    class Meta:
+        model = Profile
+        fields = ["profile_image"]
+
+        widgets = {
+            "profile_image": forms.FileInput(attrs={
+                "class": "hidden"
+            })
         }
--- clarity/users/views.py
+++ clarity/users/views.py
@@ -1,7 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.decorators import login_required
 from django.contrib.auth.views import LoginView
 from django.views.generic.edit import FormView
-from .forms import RegisterForm
+from .forms import RegisterForm, UpdateUserForm, ProfileImageForm
 from django.contrib.auth import login
 
 # Create your views here.
@@ -18,6 +19,33 @@ class Register(FormView):
         user = form.save()
         login(self.request, user)
         return super().form_valid(form)
-    
+
+@login_required    
 def profile(request):
-    return render(request, "users/profile.html")
+
+    user_form = UpdateUserForm(instance=request.user)
+    image_form = ProfileImageForm(instance=request.user.profile)
+
+    if request.method == "POST":
+
+        if "info_submit" in request.POST:
+            user_form = UpdateUserForm(request.POST, instance=request.user)
+            if user_form.is_valid():
+                user_form.save()
+                return redirect("/users/profile/")
+        
+        elif "image_submit" in request.POST:
+            image_form = ProfileImageForm(
+                request.POST, 
+                request.FILES,
+                instance=request.user.profile
+            )
+            if image_form.is_valid():
+                image_form.save()
+                return redirect("/users/profile/")
+
+
+    return render(request, "users/profile.html", {
+        "user_form": user_form,
+        "image_form": image_form
+    })
